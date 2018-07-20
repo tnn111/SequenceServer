2554f4340d8a03ef73a11c21eb526ca028a321a8
Torben Nielsen <mjoelnir@me.com>
2018-07-20 14:57:53 -0700

    Sort of working.

--- SequenceServer.py
+++ SequenceServer.py
@@ -8,9 +8,14 @@ BASE = '/Users/torben/Dropbox/Work/Python/SequenceServer/static/'
 
 @app.route('/')
 def index():
-    return 'Index'
-    
+    return 'In the beginning there was the Void.'
+
 @app.route('/<name>')
-def hello_world(name):
-    print(os.listdir(BASE))
-    return send_file(BASE + 'GCF_000675515.1.fna.gz')
+def return_file(name):
+    xxx, yyy, zzz, version = name[4:7], name[7:10], name[10:13], name[14:]
+    if version:
+      file_name = xxx + '/' + yyy + '/' + zzz + '/' + \
+                  'GCF_' + xxx + yyy + zzz + '.' + version + '/' + \
+                  'GCF_' + xxx + yyy + zzz + '.' + version + '.fna.gz'
+    app.logger.info('File name is: ' + file_name)
+    return send_file(BASE + file_name, as_attachment = True)
